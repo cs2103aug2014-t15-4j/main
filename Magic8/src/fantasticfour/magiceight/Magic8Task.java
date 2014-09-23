509e9be5f68b47080846f92bf05a66226689272e
Jeffrey Effendy <wawa_union.zone@live.com>
2014-09-23 23:37:52 +0800

    Adding Magic8Task Class and Interface - J

---
+++ Magic8/src/fantasticfour/magiceight/Magic8Task.java
@@ -0,0 +1,36 @@
+package fantasticfour.magiceight;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Magic8Task implements Magic8TaskInterface {
+	private int id;
+	private String desc;
+	private Date deadline;
+	private ArrayList<Integer> tagIds;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public ArrayList<Integer> getTagIds() {
+		return tagIds;
+	}
+	public void setTagIds(ArrayList<Integer> tagIds) {
+		this.tagIds = tagIds;
+	}
+}
---
+++ Magic8/src/fantasticfour/magiceight/Magic8TaskInterface.java
@@ -0,0 +1,18 @@
+package fantasticfour.magiceight;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public interface Magic8TaskInterface {
+	public int getId();
+	public void setId(int id);
+	
+	public String getDesc();
+	public void setDesc(String desc);
+	
+	public Date getDeadline();
+	public void setDeadline(Date deadline);
+	
+	public ArrayList<Integer> getTagIds();
+	public void setTagIds(ArrayList<Integer> tagIds);
+}
