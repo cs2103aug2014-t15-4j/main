472a2e50c837dd99cb579383d33e65d5db4f5124
Sponginess <animerocksforever@hotmail.com>
2014-09-24 01:19:02 +0800

    Added constructor for Magic8 Class. Update addTask in Magic8 Class to include printwriter.

--- Magic8/src/fantasticfour/magiceight/Magic8.java
+++ Magic8/src/fantasticfour/magiceight/Magic8.java
@@ -1,15 +1,40 @@
 package fantasticfour.magiceight;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import fantasticfour.magiceight.Magic8Task;
 
+
 public class Magic8 implements Magic8Interface {
     String fileName;
+    File file;
     ArrayList<Magic8Task> taskList;
     ArrayList<Magic8Task> bufferedTaskList;
-
+    PrintWriter pw;
+    
+    public Magic8(String fileName) {
+        this.fileName = fileName;
+        file = new File(fileName);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
     public Magic8Task addTask(Magic8Task task) {
+        try {
+            pw = new PrintWriter(fileName);
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -24,4 +49,5 @@ public class Magic8 implements Magic8Interface {
     public void displayAll() {
 
     }
+    
 }
