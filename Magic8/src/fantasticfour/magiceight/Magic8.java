1b88e71f96b3a207af2dc8d4c628a3181f168a38
Jeffrey Effendy <wawa_union.zone@live.com>
2014-09-24 00:27:58 +0800

    Add Magic8 Class - J

---
+++ Magic8/src/fantasticfour/magiceight/Magic8.java
@@ -0,0 +1,27 @@
+package fantasticfour.magiceight;
+
+import java.util.ArrayList;
+
+import fantasticfour.magiceight.Magic8Task;
+
+public class Magic8 implements Magic8Interface {
+    String fileName;
+    ArrayList<Magic8Task> taskList;
+    ArrayList<Magic8Task> bufferedTaskList;
+
+    public Magic8Task addTask(Magic8Task task) {
+        return null;
+    }
+
+    public Magic8Task removeTask(int id) {
+        return null;
+    }
+
+    public Magic8Task updateTask(Magic8Task task) {
+        return null;
+    }
+
+    public void displayAll() {
+
+    }
+}
--- Magic8/src/fantasticfour/magiceight/Magic8TaskInterface.java
+++ Magic8/src/fantasticfour/magiceight/Magic8TaskInterface.java
@@ -4,18 +4,23 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public interface Magic8TaskInterface {
-	public int getId();
-	public void setId(int id);
-	
-	public String getDesc();
-	public void setDesc(String desc);
-	
-	public Date getDeadline();
-	public void setDeadline(Date deadline);
-	
-	public ArrayList<Integer> getTagIds();
-	public void setTagIds(ArrayList<Integer> tagIds);
-	
-	public void addTagId(int id);
-	public void removeTagId(int id);
+    public int getId();
+
+    public void setId(int id);
+
+    public String getDesc();
+
+    public void setDesc(String desc);
+
+    public Date getDeadline();
+
+    public void setDeadline(Date deadline);
+
+    public ArrayList<Integer> getTagIds();
+
+    public void setTagIds(ArrayList<Integer> tagIds);
+
+    public void addTagId(int id);
+
+    public void removeTagId(int id);
 }
