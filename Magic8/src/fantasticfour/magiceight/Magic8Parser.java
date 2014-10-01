e8cd9722de1abaca44e8283a2fc75c64533bb485
bumbleblym <bumbleblym@gmail.com>
2014-10-01 22:22:11 +0800

    feat: add Magic8Parser

---
+++ Magic8/src/fantasticfour/magiceight/Magic8Parser.java
@@ -0,0 +1,7 @@
+package fantasticfour.magiceight;
+
+public class Magic8Parser implements Magic8ParserInterface {
+	public void parseCommand(String command) {
+		// TODO Call Magic8Interface Commands
+	}
+}
---
+++ Magic8/src/fantasticfour/magiceight/Magic8ParserInterface.java
@@ -0,0 +1,5 @@
+package fantasticfour.magiceight;
+
+public interface Magic8ParserInterface {
+	public void parseCommand(String command);
+}
