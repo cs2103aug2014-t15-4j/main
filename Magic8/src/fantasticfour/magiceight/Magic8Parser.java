aa24d7c78561e971cc6d50c6f008da7aef6a1981
Malavika <malavika94@yahoo.com.sg>
2014-10-06 13:19:04 +0800

    Update: smoothening changes

--- Magic8/src/fantasticfour/magiceight/Magic8Parser.java
+++ Magic8/src/fantasticfour/magiceight/Magic8Parser.java
@@ -1,9 +1,35 @@
 package fantasticfour.magiceight;
 
-public class Magic8Parser implements Magic8ParserInterface {
-	public void parseCommand(String command) {
-		// TODO Call Magic8Interface Commands
-	}
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Magic8Parser {
+    private static ArrayList<String> commandRegexList = new ArrayList<String>() {
+        {
+            add("add \\w+((\\s#\\w+)+)?( by \\w+)?");
+            add("clear");
+            add("delete (\\w+|\\d+ to \\d+|\\*");
+            add("display");
+            add("edit \\d+ \\w+");
+            add("help");
+            add("search \\w+");
+            add("undo( \\d+)+");
+        }
+    };
+    
+    public static void parseCommand(String command) {
+        // TODO Call Magic8Interface Commands
+    }
+
+    public static boolean isCommandValid(String command) {
+        for(String allowedCommandRegex : commandRegexList) {
+            if(Pattern.matches(allowedCommandRegex, command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+       
 
 	public static boolean isCommandValid(String command) {
 		// TODO Auto-generated method stub
