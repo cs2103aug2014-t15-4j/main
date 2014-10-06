169ec9a09b9bbf02392f1e6e5969f205a2c5ff64
bumbleblym <bumbleblym@gmail.com>
2014-10-06 19:40:14 +0800

    feat: add indexing of tasks by tags

--- Magic8/src/fantasticfour/magiceight/Magic8TaskList.java
+++ Magic8/src/fantasticfour/magiceight/Magic8TaskList.java
@@ -1,10 +1,12 @@
 package fantasticfour.magiceight;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 class Magic8TaskList implements Magic8TaskListInterface {
     private HashMap<Integer, Magic8Task> taskList;
     private int id;
+    private HashMap<String, HashSet<Integer>> tagToTaskIdsMap;
 
     public Magic8TaskList(HashMap<Integer, Magic8Task> taskList, int id) {
         this.taskList = taskList;
@@ -15,21 +17,28 @@ class Magic8TaskList implements Magic8TaskListInterface {
         int taskId = this.id++;
         task.setId(taskId);
         this.taskList.put(taskId, task);
+        indexTask(task);
 
         return task;
     }
 
     public Magic8Task removeTask(Magic8Task task) {
         int taskId = task.getId();
-        return this.taskList.remove(taskId);
+        Magic8Task storedTask = this.taskList.remove(taskId);
+
+        if (storedTask != null) {
+            unindexTask(task);
+        }
+
+        return storedTask;
     }
 
     public boolean updateTask(Magic8Task task) {
         boolean result = false;
 
-        int taskId = task.getId();
-        if (this.taskList.containsKey(taskId)) {
-            this.taskList.put(taskId, task);
+        Magic8Task storedTask = removeTask(task);
+        if (storedTask != null) {
+            addTask(task);
             result = true;
         }
 
@@ -41,6 +50,40 @@ class Magic8TaskList implements Magic8TaskListInterface {
 
         if (!this.taskList.isEmpty()) {
             this.taskList.clear();
+            result = true;
+        }
+
+        return result;
+    }
+
+    private boolean indexTask(Magic8Task task) {
+        boolean result = false;
+
+        unindexTask(task);
+
+        int taskId = task.getId();
+        for (String tag : task.getTags()) {
+            HashSet<Integer> taskIdsWithTag = tagToTaskIdsMap.get(tag);
+            taskIdsWithTag.add(taskId);
+        }
+
+        result = true;
+
+        return result;
+    }
+
+    private boolean unindexTask(Magic8Task task) {
+        boolean result = false;
+
+        int taskId = task.getId();
+        Magic8Task storedTask = taskList.get(taskId);
+
+        if (storedTask != null) {
+            for (String tag : storedTask.getTags()) {
+                HashSet<Integer> taskIdsWithTag = tagToTaskIdsMap.get(tag);
+                taskIdsWithTag.remove(taskId);
+            }
+
             result = true;
         }
 
