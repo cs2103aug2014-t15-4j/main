09c594f351bfdd08f8c7deeaf9f83c26f1dfa669
Sponginess <animerocksforever@hotmail.com>
2014-10-07 00:45:50 +0800

    feat: add Magic8Task toString and parse methods

--- Magic8/src/fantasticfour/magiceight/Magic8Task.java
+++ Magic8/src/fantasticfour/magiceight/Magic8Task.java
@@ -1,5 +1,8 @@
 package fantasticfour.magiceight;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashSet;
 
@@ -8,9 +11,10 @@ class Magic8Task implements Magic8TaskInterface {
     private String desc;
     private Date deadline;
     private HashSet<String> tags;
-    
-    public Magic8Task(int id, String desc, Date deadline,
-            HashSet<String> tags) {
+
+    private static DateFormat df = DateFormat.getDateInstance();
+
+    public Magic8Task(int id, String desc, Date deadline, HashSet<String> tags) {
         this.id = id;
         this.desc = desc;
         this.deadline = deadline;
@@ -60,5 +64,30 @@ class Magic8Task implements Magic8TaskInterface {
     public void replaceTag(String newTag, String oldTag) {
         this.tags.remove(oldTag);
         this.tags.add(newTag);
+    }
+
+    public String toString(String delimiter) {
+        String taskId = Integer.toString(this.id);
+        String desc = this.desc.replace(delimiter, "\\" + delimiter);
+        String deadline = df.format(this.deadline);
+        String tags = "";
+        for (String tag : this.tags) {
+            tags += " " + tag;
+        }
+        return taskId + delimiter + desc + delimiter + deadline + delimiter
+                + tags;
+    }
+
+    public static Magic8Task parse(String s, String delimiter)
+            throws ParseException {
+        String regex = "(?<!\\\\)" + delimiter;
+        String[] tokens = s.split(regex);
+
+        int taskId = Integer.parseInt(tokens[0]);
+        String desc = tokens[1];
+        Date deadline = df.parse(tokens[2]);
+        HashSet<String> tags = new HashSet(Arrays.asList(tokens[3].split(" ")));
+        Magic8Task task = new Magic8Task(taskId, desc, deadline, tags);
+        return task;
     }
 }
