d7d357ebc1c684f990b2f36c7c2335ccd7448171
bumbleblym <bumbleblym@gmail.com>
2014-10-06 17:12:12 +0800

    style: change Javadoc to meet coding standard
    Add empty line between description and parameter section
    Align parameter descriptions

--- Magic8/src/fantasticfour/magiceight/Magic8TaskListInterface.java
+++ Magic8/src/fantasticfour/magiceight/Magic8TaskListInterface.java
@@ -5,6 +5,7 @@ interface Magic8TaskListInterface {
      * Updates the task id,
      * adds the task to the task list,
      * and returns it.
+     * 
      * @param task  Task to be added.
      * @return      Task with updated id.
      */
@@ -13,22 +14,27 @@ interface Magic8TaskListInterface {
     /**
      * Removes the task with the id of the specified task from the task list,
      * and returns it
+     * 
      * @precondition    Specified task was not changed after being taken from task list.
-     * @param task  Task to be removed.
-     * @return      Task with the same id as the specified task.
+     * @param task      Task to be removed.
+     * @return          Task with the same id as the specified task.
      */
     public Magic8Task removeTask(Magic8Task task);
 
     /**
      * Updates the task with the id of the specified task in the task list.
+     * 
      * @param task  Task to replace the stored task with.
-     * @return      True if task was updated successfully.
+     * @return      true    if task was updated.
+     *              false   otherwise.
      */
     public boolean updateTask(Magic8Task task);
     
     /**
      * Removes all tasks from the task list.
-     * @return  True if tasks were removed successfully.
+     * 
+     * @return  true    if tasks were removed.
+     *          false   if task list was empty.
      */
     public boolean clearTasks();
 }
