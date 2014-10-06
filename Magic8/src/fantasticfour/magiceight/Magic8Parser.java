04c63d90dc429511feb5d80b8527d52d9fb087f0
Jeffrey Effendy <wawa_union.zone@live.com>
2014-10-06 12:10:00 +0800

    Add Magic8Parser with static method isCommandValid

--- Magic8/src/fantasticfour/magiceight/Magic8Parser.java
+++ Magic8/src/fantasticfour/magiceight/Magic8Parser.java
@@ -1,7 +1,33 @@
 package fantasticfour.magiceight;
 
-public class Magic8Parser implements Magic8ParserInterface {
-	public void parseCommand(String command) {
-		// TODO Call Magic8Interface Commands
-	}
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class Magic8Parser {
+    private static ArrayList<String> commandRegexList = new ArrayList<String>() {
+        {
+            add("add \\w+((\\s#\\w+)+)?( by \\w+)?");
+            add("clear");
+            add("delete (\\w+|\\d+ to \\d+|\\*");
+            add("display");
+            add("edit \\d+ \\w+");
+            add("help");
+            add("search \\w+");
+            add("undo( \\d+)+");
+        }
+    };
+    
+    public static void parseCommand(String command) {
+        // TODO Call Magic8Interface Commands
+    }
+
+    public static boolean isCommandValid(String command) {
+        for(String allowedCommandRegex : commandRegexList) {
+            if(Pattern.matches(allowedCommandRegex, command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+       
 }
