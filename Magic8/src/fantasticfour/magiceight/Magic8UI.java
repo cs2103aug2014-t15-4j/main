80b95158fde42cf7d78e4036e9134b7a74b30cbc
Malavika <malavika94@yahoo.com.sg>
2014-10-10 09:05:29 +0800

    feat: Add "Hello, World!" message to the Magic8 GUI

--- Magic8/src/fantasticfour/magiceight/Magic8UI.java
+++ Magic8/src/fantasticfour/magiceight/Magic8UI.java
@@ -3,20 +3,35 @@ package fantasticfour.magiceight;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
 import javax.swing.JTextPane;
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.TextArea;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
 import javax.swing.JSplitPane;
 
-public class Magic8UI {
+public class Magic8UI implements Runnable{
 
     private JFrame frame;
+    private JLabel label;
 
     /**
      * Launch the application.
      */
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
+    	Runnable app = new Magic8UI();
+    	try {
+    		SwingUtilities.invokeAndWait(app);
+    	} catch (InvocationTargetException ex) {
+    		ex.printStackTrace();
+    	} catch (InterruptedException ex) {
+    		ex.printStackTrace();
+    	}
+ /*       EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     Magic8UI window = new Magic8UI();
@@ -25,8 +40,9 @@ public class Magic8UI {
                     e.printStackTrace();
                 }
             }
-        });
+        });*/
     }
+    
 
     /**
      * Create the application.
@@ -39,10 +55,23 @@ public class Magic8UI {
      * Initialize the contents of the frame.
      */
     private void initialize() {
-        frame = new JFrame();
+        frame = new JFrame("Magic8 - V0.1");
+        label = new JLabel("Hello, world!");
+        label.setFont(new Font("SansSerif", Font.PLAIN, 20));
         frame.setBounds(100, 100, 665, 520);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+        	public void windowClosing(WindowEvent e) {
+        		frame.setVisible(false);
+        		//Perform whatever operations needed before exit
+        		System.exit(0);
+        	}
+        });
         
+        frame.add(label);
+        frame.pack();
+        frame.setVisible(true);
+        		
         JSplitPane splitPane = new JSplitPane();
         frame.getContentPane().add(splitPane, BorderLayout.NORTH);
         
