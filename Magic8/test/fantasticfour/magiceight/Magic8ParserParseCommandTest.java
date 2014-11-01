2a55483d2a908f25c672f9370f662aa6dd140039
Jeffrey Effendy <wawa_union.zone@live.com>
2014-11-02 05:58:05 +0800

    Update Magic8Task Parameters in Magic8StorageTest and remove warning messages in Parser and UI

--- Magic8/test/fantasticfour/magiceight/Magic8ParserParseCommandTest.java
+++ Magic8/test/fantasticfour/magiceight/Magic8ParserParseCommandTest.java
@@ -3,7 +3,6 @@ package fantasticfour.magiceight;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 import org.junit.Before;
 import org.junit.Test;
