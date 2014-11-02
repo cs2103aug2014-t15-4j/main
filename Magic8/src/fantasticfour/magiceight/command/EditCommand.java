3af1e0ab9b9acf8f163428822a329cda73a23922
Jeffrey Effendy <wawa_union.zone@live.com>
2014-11-02 15:15:42 +0800

    Fix: edit null pointer exception when no task is found

--- Magic8/src/fantasticfour/magiceight/command/EditCommand.java
+++ Magic8/src/fantasticfour/magiceight/command/EditCommand.java
@@ -1,6 +1,7 @@
 package fantasticfour.magiceight.command;
 
 import java.io.IOException;
+import java.util.TreeMap;
 
 import fantasticfour.magiceight.Magic8CommandObject;
 import fantasticfour.magiceight.Magic8Task;
@@ -14,9 +15,16 @@ public class EditCommand extends Command {
     
     public void execute() throws IOException {
         Magic8Task task = null;
-        task = super.getTaskManager().getAllTasks().get(super.getIds().get(0));
-        task.setDesc(super.getTaskDescription());
-        super.getTaskManager().updateTask(task);
-        System.out.println("Task is edited succesfully");
+        TreeMap<Integer, Magic8Task> tasks = null;
+        int id = super.getIds().get(0);
+        tasks = super.getTaskManager().getAllTasks();
+        if(tasks.containsKey(id)) {
+            task = tasks.get(id);
+            task.setDesc(super.getTaskDescription());
+            super.getTaskManager().updateTask(task);
+            System.out.println("Task is edited succesfully");
+        } else {
+            System.out.println("No such task is found");
+        }
     }
 }
