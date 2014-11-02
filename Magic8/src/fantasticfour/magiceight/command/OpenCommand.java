08fd602e3b855d8cfcde00c9ba6fb2d27360547f
Jeffrey Effendy <wawa_union.zone@live.com>
2014-11-03 03:16:04 +0800

    Add OpenCommand.java

---
+++ Magic8/src/fantasticfour/magiceight/command/OpenCommand.java
@@ -0,0 +1,24 @@
+package fantasticfour.magiceight.command;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import fantasticfour.magiceight.Magic8CommandObject;
+import fantasticfour.magiceight.Magic8Status;
+import fantasticfour.magiceight.Magic8TaskList;
+
+public class OpenCommand extends Command{
+    
+    public OpenCommand(Magic8CommandObject obj, Magic8TaskList tm) {
+        super(obj, tm);
+    }
+    
+    public void execute() {
+        try {
+            super.setTaskManager(new Magic8TaskList(super.getTaskDescription()));
+            this.setStatus(Magic8Status.SUCCESS);
+        } catch (IOException | ParseException e) {
+            this.setStatus(Magic8Status.ERROR);
+        }
+    }
+}
