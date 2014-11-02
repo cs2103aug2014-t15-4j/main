719f28728ec9de0abf993fa26ee188d0512cbce0
Malavika <malavika94@yahoo.com.sg>
2014-11-02 19:11:36 +0800

    feat(Magic8UI): add Calendar onto mainFrame

---
+++ Magic8/src/fantasticfour/magiceight/CalendarProgram.java
@@ -0,0 +1,194 @@
+package fantasticfour.magiceight;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class CalendarProgram {
+	static JLabel lblMonth, lblYear;
+	static JButton btnPrev, btnNext;
+	static JTable tblCalendar;
+//	static JComboBox cmbYear;
+	static Container pane;
+	static DefaultTableModel mtblCalendar; //Table model
+	static JScrollPane stblCalendar; //The scrollpane
+	static JPanel pnlCalendar;
+	static int realYear, realMonth, realDay, currentYear, currentMonth;
+
+	public static void frameSetUp(JFrame frameMagic8UI) {
+		//Look and feel
+		try {UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());}
+		catch (ClassNotFoundException e) {}
+		catch (InstantiationException e) {}
+		catch (IllegalAccessException e) {}
+		catch (UnsupportedLookAndFeelException e) {}
+
+		//Prepare frame
+		pane = frameMagic8UI.getContentPane(); //Get content pane
+		pane.setLayout(null); //Apply null layout
+
+		//Create controls
+		lblMonth = new JLabel ("January");
+//		cmbYear = new JComboBox();
+		btnPrev = new JButton ("<");
+		btnNext = new JButton (">");
+		mtblCalendar = new DefaultTableModel(){
+			public boolean isCellEditable(int rowIndex, int mColIndex){
+				return false;
+				}
+			};
+		tblCalendar = new JTable(mtblCalendar);
+		stblCalendar = new JScrollPane(tblCalendar);
+		pnlCalendar = new JPanel(null);
+		
+		//Register action listeners
+		btnPrev.addActionListener(new btnPrev_Action());
+		btnNext.addActionListener(new btnNext_Action());
+//		cmbYear.addActionListener(new cmbYear_Action());
+		
+		//Add controls to pane
+		pane.add(pnlCalendar);
+		pnlCalendar.add(lblMonth);
+		pnlCalendar.add(btnPrev);
+		pnlCalendar.add(btnNext);
+		pnlCalendar.add(stblCalendar);
+		
+		//Set bounds
+		pnlCalendar.setBounds(410, 45, 300, 200);
+//		lblMonth.setBounds(140-lblMonth.getPreferredSize().width/2, 15, 20, 5);
+		btnPrev.setBounds(10, 15, 20, 20);
+		btnNext.setBounds(270, 15, 20, 20);
+		stblCalendar.setBounds(0, 35, 300, 200);
+	
+		
+		//Get real month/year
+		GregorianCalendar cal = new GregorianCalendar(); //Create calendar
+		realDay = cal.get(GregorianCalendar.DAY_OF_MONTH); //Get day
+		realMonth = cal.get(GregorianCalendar.MONTH); //Get month
+		realYear = cal.get(GregorianCalendar.YEAR); //Get year
+		currentMonth = realMonth; //Match month and year
+		currentYear = realYear;
+		
+		//Add headers
+		String[] headers = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}; //All headers
+		for (int i=0; i<7; i++){
+			mtblCalendar.addColumn(headers[i]);
+		}
+		
+		tblCalendar.getParent().setBackground(tblCalendar.getBackground()); //Set background
+
+		//No resize/reorder
+		tblCalendar.getTableHeader().setResizingAllowed(false);
+		tblCalendar.getTableHeader().setReorderingAllowed(false);
+
+		//Single cell selection
+		tblCalendar.setColumnSelectionAllowed(true);
+		tblCalendar.setRowSelectionAllowed(true);
+		tblCalendar.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		//Set row/column count
+		tblCalendar.setRowHeight(20);
+		tblCalendar.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		mtblCalendar.setColumnCount(7);
+		mtblCalendar.setRowCount(6);
+		
+		tblCalendar.getColumnModel().getColumn(0).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(1).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(2).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(3).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(4).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(5).setPreferredWidth(10);
+		tblCalendar.getColumnModel().getColumn(6).setPreferredWidth(10);
+		
+		//Refresh calendar
+		refreshCalendar (realMonth, realYear); //Refresh calendar
+	}
+	
+	public static void refreshCalendar(int month, int year){
+		//Variables
+		String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+		int nod, som; //Number Of Days, Start Of Month
+			
+		//Allow/disallow buttons
+		btnPrev.setEnabled(true);
+		btnNext.setEnabled(true);
+		if (month == 0 && year <= realYear-10){btnPrev.setEnabled(false);} //Too early
+		if (month == 11 && year >= realYear+100){btnNext.setEnabled(false);} //Too late
+		lblMonth.setText(months[month]); //Refresh the month label (at the top)
+		lblMonth.setBounds(150-lblMonth.getPreferredSize().width/2, 5, 180, 25); //Re-align label with calendar
+		
+		//Clear table
+		for (int i=0; i<6; i++){
+			for (int j=0; j<7; j++){
+				mtblCalendar.setValueAt(null, i, j);
+			}
+		}
+		
+		//Get first day of month and number of days
+		GregorianCalendar cal = new GregorianCalendar(year, month, 1);
+		nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+		som = cal.get(GregorianCalendar.DAY_OF_WEEK);
+		
+		//Draw calendar
+		for (int i=1; i<=nod; i++){
+			int row = new Integer((i+som-2)/7);
+			int column  =  (i+som-2)%7;
+			mtblCalendar.setValueAt(i, row, column);
+		}
+
+		//Apply renderers
+		tblCalendar.setDefaultRenderer(tblCalendar.getColumnClass(0), new tblCalendarRenderer());
+	}
+
+	static class tblCalendarRenderer extends DefaultTableCellRenderer{
+		private static final long serialVersionUID = 1L;
+
+		public Component getTableCellRendererComponent (JTable table, Object value, boolean selected, boolean focused, int row, int column){
+			super.getTableCellRendererComponent(table, value, selected, focused, row, column);
+			if (column == 0 || column == 6){ //Week-end
+				setBackground(new Color(255, 220, 220));
+			}
+			else{ //Week
+				setBackground(new Color(255, 255, 255));
+			}
+			if (value != null){
+				if (Integer.parseInt(value.toString()) == realDay && currentMonth == realMonth && currentYear == realYear){ //Today
+					setBackground(new Color(220, 220, 255));
+				}
+			}
+			setBorder(null);
+			setForeground(Color.black);
+			return this;  
+		}
+	}
+
+	static class btnPrev_Action implements ActionListener{
+		public void actionPerformed (ActionEvent e){
+			if (currentMonth == 0){ //Back one year
+				currentMonth = 11;
+				currentYear -= 1;
+			}
+			else{ //Back one month
+				currentMonth -= 1;
+			}
+			refreshCalendar(currentMonth, currentYear);
+		}
+	}
+	
+	static class btnNext_Action implements ActionListener{
+		public void actionPerformed (ActionEvent e){
+			if (currentMonth == 11){ //Foward one year
+				currentMonth = 0;
+				currentYear += 1;
+			}
+			else{ //Foward one month
+				currentMonth += 1;
+			}
+			refreshCalendar(currentMonth, currentYear);
+		}
+	}
+	
+}
